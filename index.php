b8c0b39dd943fce99c4ce8c420f0d8c54ac9413c
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-01 15:10:01 +0200

    Created style and html card

--- index.php
+++ index.php
@@ -55,9 +55,49 @@ $discs = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP DISCHI</title>
 
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
     <!-- STYLE -->
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+    <!-- HEADER -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <!-- Header Content Area -->
+                    <div>
+                        <figure class="m-0">
+                            <img src="../images/logo.svg" alt="logo">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex flex-wrap">
+                        <div class="card">
+                            <figure class="m-0 h-50">
+                                <img src="" alt="">
+                            </figure>
+                            <div class="h-50">
+                                <h1 class="mb-3"></h1>
+                                <p></p>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
