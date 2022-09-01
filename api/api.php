a46b086089b431b0589f9eaf00aa3e9ac8e3b2b3
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-01 16:19:27 +0200

    Add includes folder for php data

---
+++ api/api.php
@@ -0,0 +1,18 @@
+<?php
+
+$results = $discs;
+$search = $_GET['search'] ?? '';
+
+if($search !== ''){
+    foreach($discs as $disc){
+        $results = [];
+        $title = strtolower($disc['title']);
+        $searched_term = strtolower($search);
+        if(str_pos($title,$searched_term) !== false) $results[] = $disc;
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($results);
+
+?>
---
+++ prog-vue/script.js
@@ -0,0 +1,19 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+  el: "#root",
+  data: {
+    movies: [],
+    search: "",
+  },
+  methods: {
+    fetchData() {
+      axios.get("http://localhost/8888/demo/php-ajax-dischi/index.php").then(() => {
+        this.movies = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+});
